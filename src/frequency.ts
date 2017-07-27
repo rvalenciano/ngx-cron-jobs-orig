a896c99e3503b9cf9f2fb03e7743f60738a16af8
Randall Valenciano Fallas <ravf.226@gmail.com>
2017-07-26 18:30:33 -0600

    Master: initial commit

---
+++ src/frequency.ts
@@ -0,0 +1,8 @@
+export class Frequency {
+    base: Number;
+    minuteValues: Array<Number>;
+    hourValues: Array<Number>;
+    dayOfMonthValues: Array<Number>;
+    monthValues: Array<Number>;
+    dayValues: Array<Number>;
+}
