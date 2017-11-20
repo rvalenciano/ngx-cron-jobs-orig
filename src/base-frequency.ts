5eab8fb4e3e099396466dc238ac2b4d05cffc985
Daniel 'yp2' Derezinski <daniel.derezinski@gmail.com>
2017-11-20 14:14:51 +0100

    Merge with master and cleanup

--- src/base-frequency.ts
+++
@@ -1,8 +0,0 @@
-export class BaseFrequency {
-    minute: Number;
-    hour: Number;
-    day: Number;
-    week: Number;
-    month: Number;
-    year: Number;
-}
--- src/frequency.ts
+++
@@ -1,8 +0,0 @@
-export class Frequency {
-    base: Number;
-    minuteValues: Array<Number>;
-    hourValues: Array<Number>;
-    dayOfMonthValues: Array<Number>;
-    monthValues: Array<Number>;
-    dayValues: Array<Number>;
-}
